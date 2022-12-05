5b8cc998fe0b8fac374ba41d12d13770cc360c58
Stanislav Shadrov <Stasgrisha2@yandex.ru>
2022-12-05 18:57:29 +0300

    new folder added

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 
+
+Random smt= new Random(); 
+
+int abc = smt.Next(100,1000);
+
+int res = (abc/10)%100;
+
+Console.WriteLine(abc);
+Console.WriteLine(res);*/
