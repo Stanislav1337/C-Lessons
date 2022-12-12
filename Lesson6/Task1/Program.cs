78222653150802d1431049f02ecfed15d2c39758
Stanislav <stasgrisha2@yandex.ru>
2022-12-12 19:55:02 +0300

    HM6 is done

---
+++ Lesson6/Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/* 0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+ */
+
+// using static UserMethods.Method;
+
+// Console.WriteLine($"How many number you want entered?");
+// int usersLenghtArray = InputIntNumberTryParse();
+// while(usersLenghtArray <= 0)
+// {
+//     Console.WriteLine($"Entered unacceptable number. Need number > 0");
+//     usersLenghtArray = InputIntNumberTryParse(); 
+// }
+
+// int[] arrayEnteredUsersNumbers = CreatedArrayUserEntered(usersLenghtArray);
+// int result = CounterNumbersMoreThenZeroForUserEnter(arrayEnteredUsersNumbers);
+
+// Console.WriteLine($"{result} -- ammount numbers > 0");
+
+
+
+// int[] CreatedArrayUserEntered (int enteredNumber)
+// {
+//     int[] array = new int[enteredNumber];
+//     for (int i = 0; i < array.Length; i++)
+//     {   
+//         Console.Write($"Enter [{i+1}] number: "); 
+//         array[i] = InputIntNumberTryParse();
+//     }
+//     return array;
+// }
+
+// int CounterNumbersMoreThenZeroForUserEnter(int[] enteredArray)
+// {
+//     int count = 0;
+//     for(int i = 0; i < enteredArray.Length; i++)
+//     {
+//         if (enteredArray[i] > 0) count++;
+//     }
+//     return count;
+// }
---
+++ Lesson6/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+// using static UserMethods.Method;
+
+// Console.WriteLine("y = k1 * x + b1, y = k2 * x + b2;");
+
+// Console.Write("Enter k1: ");
+// double k1 = InputDoubleNumberTryParse();
+// Console.Write("Enter b1: ");
+// double b1 = InputDoubleNumberTryParse();
+// Console.Write("Enter k2: ");
+// double k2 = InputDoubleNumberTryParse();
+// Console.Write("Enter b2: ");
+// double b2 = InputDoubleNumberTryParse();
+// Console.WriteLine($"y = {k1} * x + {b1}, y = {k2} * x + {b2};");
+// Console.WriteLine("");
+
+// while (k1 - k2 == 0) 
+// {
+//     Console.WriteLine("k1 - k2 = 0. Enter again k1 and k2.");
+//     Console.Write("Enter k1: ");
+//     k1 = InputDoubleNumberTryParse();
+//     Console.Write("Enter k2: ");
+//     k2 = InputDoubleNumberTryParse();
+// };
+// double x = (b2 - b1) / (k1 - k2);
+// double y = k2 * x + b2;
+// Console.WriteLine($"(x, y) = ({x}, {y})");
+
